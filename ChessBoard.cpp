b9e960d666c1b8f7a616ae98fbe51d4106db2842
Edward Cromwell <edwardlc@umich.edu>
2025-04-19 08:31:03 -0400

    feat (ChessBoard): WIP began tracking game state

--- ChessBoard.cpp
+++ ChessBoard.cpp
@@ -5,8 +5,14 @@ ChessBoard::ChessBoard()
 	init_board();
 }
 
+ChessBoard::ChessBoard(const std::string& fen)
+{
+	init_fen_board(fen);
+}
+
 void ChessBoard::init_board()
 {
+	// Initialize the bitboards for each piece type
 	bitboards[WHITE_PAWN] = Bitboard(WHITE, PAWN);
 	bitboards[WHITE_KNIGHT] = Bitboard(WHITE, KNIGHT);
 	bitboards[WHITE_BISHOP] = Bitboard(WHITE, BISHOP);
@@ -19,4 +25,19 @@ void ChessBoard::init_board()
 	bitboards[BLACK_ROOK] = Bitboard(BLACK, ROOK);
 	bitboards[BLACK_QUEEN] = Bitboard(BLACK, QUEEN);
 	bitboards[BLACK_KING] = Bitboard(BLACK, KING);
+
+	// Set the state of the initial position
+	active_color = WHITE;
+	white_kingside_castle = true;
+	white_queenside_castle = true;
+	black_kingside_castle = true;
+	black_queenside_castle = true;
+	en_passant_target_square = "";
+	halfmove_clock = 0;
+	fullmove_number = 1;
+}
+
+void ChessBoard::init_fen_board(const std::string& fen)
+{
+
 }
--- ChessBoard.hpp
+++ ChessBoard.hpp
@@ -6,11 +6,19 @@
 #include "ChessTypes.hpp"
 #include "Bitboard.hpp"
 #include <array>
+#include <string>
 
 class ChessBoard
 {
 private:
-	
+	Color active_color;
+	bool white_kingside_castle;
+	bool white_queenside_castle;
+	bool black_kingside_castle;
+	bool black_queenside_castle;
+	std::string en_passant_target_square;
+	int halfmove_clock;
+	int fullmove_number;
 public:
 	std::array<Bitboard, 12> bitboards; // 0-5 for white, 6-11 for black
 
@@ -21,10 +29,28 @@ public:
 	 */
 	ChessBoard();
 
+	/**
+	 * @brief Constructor for ChessBoard with a given FEN string.
+	 *
+	 * @param fen The FEN string to initialize the chessboard with.
+	 * @details This constructor allows for the creation of a ChessBoard from a specific position represented in FEN format.
+	 */
+	ChessBoard(const std::string& fen);
+
 	/*
-	 * @brief
+	 * @brief Initializes the chessboard with the starting position.
+	 * 
+	 * @details This function sets up the initial position of the chess pieces on the board.
 	 */
 	void init_board();
+
+	/**
+	 * @brief Initializes the chessboard with a given FEN string.
+	 * 
+	 * @param fen The FEN string to initialize the chessboard with.
+	 * @details This function parses the FEN string and sets up the board accordingly.
+	 */
+	void init_fen_board(const std::string& fen);
 };
 
 #endif // CHESSBOARD_HPP
