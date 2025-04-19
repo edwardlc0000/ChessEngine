b1212f62f03466d76a7413a0a4e4d39faf109f44
Edward Cromwell <edwardlc@umich.edu>
2025-04-19 19:57:30 -0400

    feat (ChessTypes): added helper contstans for ranks and files

--- ChessTypes.hpp
+++ ChessTypes.hpp
@@ -40,4 +40,22 @@ enum Piece
 	BLACK_KING = BLACK + KING
 };
 
+const U64 A_FILE = 0x0101010101010101;
+const U64 B_FILE = A_FILE << 1;
+const U64 C_FILE = B_FILE << 1;
+const U64 D_FILE = C_FILE << 1;
+const U64 E_FILE = D_FILE << 1;
+const U64 F_FILE = E_FILE << 1;
+const U64 G_FILE = F_FILE << 1;
+const U64 H_FILE = G_FILE << 1;
+
+const U64 RANK_1 = 0x00000000000000FF;
+const U64 RANK_2 = RANK_1 << 8;
+const U64 RANK_3 = RANK_2 << 8;
+const U64 RANK_4 = RANK_3 << 8;
+const U64 RANK_5 = RANK_4 << 8;
+const U64 RANK_6 = RANK_5 << 8;
+const U64 RANK_7 = RANK_6 << 8;
+const U64 RANK_8 = RANK_7 << 8;
+
 #endif // CHESSTYPES_HPP
